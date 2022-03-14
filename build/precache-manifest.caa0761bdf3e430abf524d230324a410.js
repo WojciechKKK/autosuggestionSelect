7a9a04bc8517c946d314c6853d81ca51a99fc2ad
WojciechKKK <wojte_kk@wp.pl>
2022-03-14 21:46:14 +0100

    add build folder

---
+++ build/precache-manifest.caa0761bdf3e430abf524d230324a410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96da4b9107d21c4f80f6a5430c155e75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1c2926248bd68be9d8a",
+    "url": "./static/css/main.cd765ea3.chunk.css"
+  },
+  {
+    "revision": "f65e60ebc462f9bcc8e1",
+    "url": "./static/js/2.63ee33b0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.63ee33b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c2926248bd68be9d8a",
+    "url": "./static/js/main.d8ace916.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
